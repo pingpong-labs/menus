e2f4b6a74b67050fd14ca56ba5a69f066b6531f0
gravitano <gravitano16@gmail.com>
2015-02-05 15:36:45 +0700

    Update config file

---
+++ src/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'styles' => array(
+        'navbar' => 'Pingpong\Menus\Presenters\Bootstrap\NavbarPresenter',
+        'navbar-right' => 'Pingpong\Menus\Presenters\Bootstrap\NavbarRightPresenter',
+        'nav-pills' => 'Pingpong\Menus\Presenters\Bootstrap\NavPillsPresenter',
+        'nav-tab' => 'Pingpong\Menus\Presenters\Bootstrap\NavTabPresenter',
+    )
+);
