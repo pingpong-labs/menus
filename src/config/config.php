98f521e7eb0f95792708a9cdd8ce97ede8504768
Maher El Gamil <maherbusnes@islamhouse.com>
2015-11-20 11:28:05 +0200

    fix code style

--- src/config/config.php
+++ src/config/config.php
@@ -1,16 +1,16 @@
 <?php
 
-return array(
+return [
 
-    'styles' => array(
-        'navbar' => 'Pingpong\Menus\Presenters\Bootstrap\NavbarPresenter',
+    'styles' => [
+        'navbar'       => 'Pingpong\Menus\Presenters\Bootstrap\NavbarPresenter',
         'navbar-right' => 'Pingpong\Menus\Presenters\Bootstrap\NavbarRightPresenter',
-        'nav-pills' => 'Pingpong\Menus\Presenters\Bootstrap\NavPillsPresenter',
-        'nav-tab' => 'Pingpong\Menus\Presenters\Bootstrap\NavTabPresenter',
-        'sidebar' => 'Pingpong\Menus\Presenters\Bootstrap\SidebarMenuPresenter',
-        'navmenu' => 'Pingpong\Menus\Presenters\Bootstrap\NavMenuPresenter',
-    ),
+        'nav-pills'    => 'Pingpong\Menus\Presenters\Bootstrap\NavPillsPresenter',
+        'nav-tab'      => 'Pingpong\Menus\Presenters\Bootstrap\NavTabPresenter',
+        'sidebar'      => 'Pingpong\Menus\Presenters\Bootstrap\SidebarMenuPresenter',
+        'navmenu'      => 'Pingpong\Menus\Presenters\Bootstrap\NavMenuPresenter',
+    ],
 
     'ordering' => false,
 
-);
+];
